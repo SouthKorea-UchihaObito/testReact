55d715f5c35e8364f1c816acf5898ca2b5944b32
UchihaObito <ctallhack@naver.com>
2024-02-21 00:58:33 +0900

    [react]
    ajax axios 활용한 데이터 받아오기 완료

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,34 +1,69 @@
 import { useState } from 'react';
 import keyboardData from '../data.js';
-import {Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import ProductCard from '../components/productCard.js';
 import axios from 'axios';
-function Main(){
+function Main() {
     let [product, setProduct] = useState(keyboardData)
-    return(
+
+
+    let [timeText, setTimeText] = useState(false);
+    let [lastProduct, setLastProduct] = useState(false);
+
+    let [clickCount, setClickCount] = useState(0);
+    return (
         <main>
             <div className='mainBg'>
 
             </div>
             <Container className='productGroup'>
                 <Row>
-                {
-                    product.map((item, index)=>{
-                    return (
-                        <ProductCard product={product[index]} index={index} key={index} />
-                    )
-                    })
-                }
+                    {
+                        product.map((item, index) => {
+                            return (
+                                <ProductCard product={product[index]} index={index} key={index} />
+                            )
+                        })
+                    }
                 </Row>
             </Container>
-            <button onClick={()=>{
-                axios.get('https://codingapple1.github.io/shop/data2.json')
-                .then((result)=>{ // 데이터 출력
-                    console.log(result.data)
-                })
-                .catch(()=>{ // ajax 요청 실패 할경우
-                    console.log('실패')
-                })
+            {timeText === true ? <p>로딩중입니다.</p> : null }
+            {lastProduct === true ? <p>더 이상 상품이 존재하지 않습니다.</p> : null}
+            <button onClick={() => {
+                setClickCount(clickCount += 1);
+                setTimeText(true);
+                if(clickCount === 1){
+                    axios.get('https://codingapple1.github.io/shop/data2.json')
+                    .then((result) => { // 데이터 출력
+                        let copy = [...product];
+                        setProduct(copy.concat(result.data));
+                        setTimeText(false);
+                        console.log(clickCount)
+    
+    
+                    })
+                    .catch(() => { // ajax 요청 실패 할경우
+                        console.log('실패')
+                    })
+                } else if(clickCount === 2) {
+                    axios.get('https://codingapple1.github.io/shop/data3.json')
+                    .then((result) => { // 데이터 출력
+                        let copy = [...product];
+                        setProduct(copy.concat(result.data));
+                        setTimeText(false);
+                        console.log(clickCount)
+    
+    
+                    })
+                    .catch(() => { // ajax 요청 실패 할경우
+                        console.log('실패')
+                    })
+                } else {
+                    setTimeText(false);
+                    setLastProduct(true);
+                    return ;
+                }
+                
             }}>버튼</button>
         </main>
     )
