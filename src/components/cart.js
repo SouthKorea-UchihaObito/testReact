64e436c10a806b35f6858e4d331cd7306d656042
SouthKorea-UchihaObito <ctallhack@naver.com>
2024-03-11 20:51:28 +0900

    [react]
    store 에 있는 array,object 변경 해야하는 방법 복습해야함

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,25 +1,25 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./styles/cart.module.scss"
+import { changeName, countPlus } from "../store.js";
+
 
 function Cart(){
-
-    // Redux
     /*
         Props 없이 state 공유 가능!
-        
         Redux Store의 state 꺼내는 법
-
     */
-
     // 변수로 일반적으로 선언
     let storeState = useSelector((storeState)=>{ // Redux store 가져옴
         // return 앞에 있는 storeState는 store안에 있던 모든 state가 됩니다.
         return storeState
         // return storeState.user <- 이런식으로 특정 항목만 따로 쓸수도 있다.
     }) 
-    console.log(storeState.productInfo);
+
+    // 3. 만든 함수 import 해서 사용 하려면 이것도 필요함
+    let dispatch = useDispatch() // useDispatch -> store.js로 요청 보내주는 함수
     return (
         <div className={styles.cart}>
+            <p>{storeState.user}의 정보</p>
             <table>
                 <caption>상품목록</caption>
                 <colgroup>
@@ -30,7 +30,7 @@ function Cart(){
                 </colgroup>
                 <thead>
                     <tr>
-                        <th>1</th>
+                        <th>목록</th>
                         <th>상품명</th>
                         <th>수량</th>
                         <th>변경하기</th>
@@ -44,7 +44,13 @@ function Cart(){
                                     <td>{index}</td>
                                     <td>{item.name}</td>
                                     <td>{item.count}</td>
-                                    <td><button type="button">변경하기</button></td>
+                                    <td><button type="button" onClick={()=>{
+                                        // 3. 만든 함수 import 해서 사용
+                                        dispatch(countPlus()); // 이렇게 사용해야함
+                                        /*
+                                            dispatch(state변경함수()) 이렇게 감싸서 사용
+                                        */ 
+                                    }}>+</button></td>
                                 </tr>
                             )
                         })
--- src/store.js
+++ src/store.js
@@ -13,8 +13,24 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 // 값 변수로 저장
 let user = createSlice({
   name : 'User',
-  initialState : 'Shim Jae Hun'  
+  initialState : 'Shim Jae Hun',
+  // Redux의 state 변경 하는 방법
+  // 1. state 수정해주는 함수 만든다.
+  // 2. 만든 함수 export 해야함
+  // 3. 원할 때 그 함수 실행해달라고 store.js 에 요청해야함
+  
+  // 1. state 수정해주는 함수 만들기
+  reducers : { // 스펠링 틀리지 않게 주의!
+    changeName(state){ // 기존 state 필요할 경우 파라미터에 넣을 수 있음
+      return 'ㄴㅇ' // 수정할 내용 으로 state 값이 변경 됨
+      // state은 기존 state 값임 ex:) Shim Jae Hun
+    }
+    // 아래에 함수 또 만들수 있음
+  }
 })
+
+
+
 
 let stock = createSlice({
   name : 'stock',
@@ -26,8 +42,20 @@ let productInfo = createSlice({
   initialState : [
     {id : 0, name : 'White and Black', count : 2},
     {id : 2, name : 'Grey Yordan', count : 1}
-  ]
+  ],
+  reducers : {
+    countPlus(state){
+      return 'asdsa'
+    }
+  }
 })
+
+// 2.만든 함수 export
+export let { changeName, countPlus } = user.actions // 오른쪽에 있는 자료를 변수로 쉽게 처리 
+// 이런 식으로 밖으로 빼놓음 함수는 여러개 넣을수 있음 오브젝트 형식으로 출력됨
+
+
+// 3. 만든 함수 import 해서 사용 (이건 사용할 곳에서 해야함)
 
 export const store = configureStore({
   reducer: {
